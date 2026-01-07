a32b76427e3c660582b208eb25fbb76c21b09c71
zinzo <zinzoddari@gmail.com>
2026-01-07 22:35:20 +0900

    feat: WordIndex create 리팩토링

--- src/main/kotlin/wordle/WordIndex.kt
+++ src/main/kotlin/wordle/WordIndex.kt
@@ -6,12 +6,14 @@ data class WordIndex(
     val value: Long
 ) {
     companion object {
+        val BASE_DATE: LocalDate = LocalDate.of(2021, 6, 19)
+
         fun create(date: LocalDate, arraySize: Int): WordIndex {
-            val baseDate: LocalDate = LocalDate.of(2021, 6, 19)
+            return extract(date, arraySize)
+        }
 
-            val result: Long = (date.toEpochDay() - baseDate.toEpochDay()) % arraySize
-
-            return WordIndex(result)
+        private fun extract(date: LocalDate, arraySize: Int): WordIndex {
+            return WordIndex((date.toEpochDay() - BASE_DATE.toEpochDay()) % arraySize)
         }
     }
 }
