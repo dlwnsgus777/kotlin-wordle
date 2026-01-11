e4dc35a2a36c470e245930821d9f25ef3766387c
zinzo <zinzoddari@gmail.com>
2026-01-11 14:15:04 +0900

    feat: 입력된 인덱스와 입력된 캐릭터가, Word와 동일한지 판단하는 로직 구현

--- src/main/kotlin/wordle/Word.kt
+++ src/main/kotlin/wordle/Word.kt
@@ -3,4 +3,7 @@ package wordle
 data class Word(
     val value: String
 ) {
+    fun check(index: Int, char: Char): Boolean {
+        return value.toCharArray().get(index) == char
+    }
 }
--- src/test/kotlin/wordle/WordTest.kt
+++ src/test/kotlin/wordle/WordTest.kt
@@ -5,4 +5,20 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class WordTest {
+
+    @Test
+    @DisplayName("입력된 인덱스와 입력된 캐릭터가, Word와 동일한지 판단한다")
+    fun test01() {
+        // arrange
+        val input: Word = Word("APPLE")
+
+        val index: Int = 0
+        val char: Char = 'A'
+
+        // act
+        val sut: Boolean = input.check(index, char)
+
+        // assert
+        assertThat(sut).isTrue
+    }
 }
