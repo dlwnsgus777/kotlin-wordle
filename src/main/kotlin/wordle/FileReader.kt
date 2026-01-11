e4ba1c9004d5bf108a8fc69cf4954df159bb18d7
zinzo <zinzoddari@gmail.com>
2026-01-11 12:15:21 +0900

    feat: List<String>으로 변환하는 로직 구현

--- src/main/kotlin/wordle/FileReader.kt
+++ src/main/kotlin/wordle/FileReader.kt
@@ -4,12 +4,18 @@ import java.io.File
 
 class FileReader {
     fun read(name: String): List<String> {
-        throw RuntimeException("Not implemented")
+        val file = readFile(name)
+        val list = mutableListOf<String>()
+        file.forEachLine {
+            list.add(it.trim())
+        }
+        return list
     }
 
     private fun readFile(name: String): File {
         val classLoader = javaClass.classLoader
-        val result = File(classLoader.getResource(name).file)
+        val resource = classLoader.getResource(name) ?: throw IllegalArgumentException("File does not exist: $name")
+        val result = File(resource.file)
         if (!result.exists()) {
             throw IllegalArgumentException("File does not exist: $name")
         }
