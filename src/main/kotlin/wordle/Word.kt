bb4610159edc1f81932705c7ff41afbec7d23d88
zinzo <zinzoddari@gmail.com>
2026-01-11 13:58:14 +0900

    feat: 문자열의 총 등장 갯수를 저장하는 map 구현

--- src/main/kotlin/wordle/Word.kt
+++ src/main/kotlin/wordle/Word.kt
@@ -1,6 +1,19 @@
 package wordle
 
 data class Word(
-    private val word: String
+    private val word: String,
+    private val counter: Map<Char, Int>
 ) {
+    constructor(word: String) : this(word, init(word))
+
+    fun getCount(char: Char): Int {
+        return counter[char] ?: 0
+    }
+
+    companion object {
+        private fun init(word: String): Map<Char, Int> {
+            return word.groupBy { it }
+                .mapValues { (_, v) -> v.size }
+        }
+    }
 }
