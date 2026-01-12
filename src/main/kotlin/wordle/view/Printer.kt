7626f6a826f8478e6bd13d7b299c5670b9c5bdc6
zinzo <zinzoddari@gmail.com>
2026-01-12 22:51:09 +0900

    refactor: 출력 입력기 수정

--- src/main/kotlin/wordle/view/Printer.kt
+++ src/main/kotlin/wordle/view/Printer.kt
@@ -1,9 +1,26 @@
 package wordle.view
 
-class Printer {
+import wordle.Results
+
+object Printer {
 
     fun introduce() {
         println("WORDLE을 6번 만에 맞춰 보세요.\n" +
                 "시도의 결과는 타일의 색 변화로 나타납니다.")
+    }
+
+    fun requestInput() {
+        println("정답을 입력해 주세요.")
+    }
+
+    fun viewTile(tile: String) {
+        println()
+        println(tile)
+        println()
+    }
+
+    fun result(count: Int, currentCount: Int, tile: String) {
+        println("${currentCount}/${count}")
+        viewTile(tile)
     }
 }
