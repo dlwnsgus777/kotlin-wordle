71f5036347f18d1d7cabf4e6cba9f762023b1630
dlwnsgus777 <dlwnsgus777@naver.com>
2026-01-30 10:54:33 +0900

    refact: 중복 검증 제거

--- src/main/kotlin/wordle/infrastructure/FileReader.kt
+++ src/main/kotlin/wordle/infrastructure/FileReader.kt
@@ -16,9 +16,6 @@ object FileReader {
         val classLoader = javaClass.classLoader
         val resource = classLoader.getResource(name) ?: throw IllegalArgumentException("파일이 존재하지 않습니다: $name")
         val result = File(resource.file)
-        if (!result.exists()) {
-            throw IllegalArgumentException("파일이 존재하지 않습니다: $name")
-        }
         return result
     }
 }
